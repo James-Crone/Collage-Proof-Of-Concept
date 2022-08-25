4af99a4780e0456c5e9da5cabad342c611791795
James-Crone <78932463+James-Crone@users.noreply.github.com>
2022-08-25 16:41:46 +0100

    Add files via upload

---
+++ C#/Array/Array/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int[] Numbers = { 4, 2, 7, 8, 12, 31 };
+
+        private void Allvalues_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                textBox1.Text += Numbers[i] + "\r\n";
+            }
+        }
+
+        private void Maxvalue_Click(object sender, EventArgs e)
+        {
+            int Max = Numbers.Max();
+            textBox1.Text = Max.ToString();
+        }
+
+        private void Minvalue_Click(object sender, EventArgs e)
+        {
+            int Min = Numbers.Min();
+            textBox1.Text = Min.ToString();
+        }
+
+        private void Totalvalue_Click(object sender, EventArgs e)
+        {
+            //int Total = Numbers[0] + Numbers[1] + Numbers[2] + Numbers[3] + Numbers[4] + Numbers[5]; wrote it this way then seen there was a .sum method
+            int Total = Numbers.Sum();
+            textBox1.Text = Total.ToString();
+        }
+    }
+}
---
+++ C#/CalculateArea/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CalculateArea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int height = Convert.ToInt32(heightTextbox.Text);
+            int width = Convert.ToInt32(widthTextbox.Text);
+
+            int area = height * width;
+            outputTextbox.Text = "The area is: " + area;
+        }
+    }
+}
